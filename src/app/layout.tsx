3d57890b6ff8eef4df26fa9472ca422b7f8bb974
Tiago Araujo <tiagomuniz130@gmail.com>
2023-11-15 15:42:29 -0300

    feat: building header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,22 @@ function RootLayout({ children }: { children: React.ReactNode }) {
         {/* <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism-funky.min.css" rel="stylesheet" /> */}
       </head>
       <body className={`${space_grotesk.variable} scroll-smooth bg-gray-950`}>
+        <header className="flex items-center justify-between">
+          <div className="flex items-center justify-center">
+            <span className="mr-2 text-2xl font-extrabold text-white">
+              &lt;tma_blog /&gt;
+            </span>
+            <span className="cursor-block" />
+          </div>
+          <nav>
+            <ul>
+              <li>home</li>
+              <li>blog</li>
+              <li>projects</li>
+              <li>about</li>
+            </ul>
+          </nav>
+        </header>
         {children}
         {/* <Script src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-core.min.js' /> */}
         {/* <Script src='https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/plugins/autoloader/prism-autoloader.min.js' /> */}
