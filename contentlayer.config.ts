a9e597878f755cae46f28871ad1d0ce5d1d17f32
Tiago Araujo <tiagomuniz130@gmail.com>
2023-11-16 06:40:59 -0300

    lint: format doc

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,35 +1,35 @@
-import { defineDocumentType, makeSource } from 'contentlayer/source-files'
-import rehypeSlug from 'rehype-slug'
-import remarkGfm from 'remark-gfm'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import rehypePrismPlus from 'rehype-prism-plus'
-import remarkCodeTitle from 'remark-code-title'
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypePrismPlus from "rehype-prism-plus";
+import rehypeSlug from "rehype-slug";
+import remarkCodeTitle from "remark-code-title";
+import remarkGfm from "remark-gfm";
 
 export const Post = defineDocumentType(() => ({
-  name: 'Post',
-  filePathPattern: `**/*.mdx`,
-  contentType: 'mdx',
+  name: "Post",
+  filePathPattern: "**/*.mdx",
+  contentType: "mdx",
   fields: {
-    title: { type: 'string', required: true },
-    date: { type: 'date', required: true },
+    title: { type: "string", required: true },
+    date: { type: "date", required: true },
   },
   computedFields: {
-    url: { type: 'string', resolve: (post) => `/posts/${post._raw.flattenedPath}` },
+    url: {
+      type: "string",
+      resolve: (post) => `posts/${post._raw.flattenedPath}`,
+    },
   },
-}))
+}));
 
 export default makeSource({
-  contentDirPath: 'posts',
+  contentDirPath: "posts",
   documentTypes: [Post],
   mdx: {
-    remarkPlugins: [
-      remarkGfm,
-      remarkCodeTitle,
-    ],
+    remarkPlugins: [remarkGfm, remarkCodeTitle],
     rehypePlugins: [
       rehypeSlug,
       rehypeAutolinkHeadings,
-      [rehypePrismPlus, { defaultLanguage: 'js', ignoreMissing: true }],
+      [rehypePrismPlus, { defaultLanguage: "js", ignoreMissing: true }],
     ],
   },
-})
+});
