c3257ba9e0ed6adcd46c44040a72779ac3f940b7
Tiago Araujo <tiagomuniz130@gmail.com>
2023-12-01 06:52:46 -0300

    style: resize dots and always keep visible

--- src/components/Dots.tsx
+++ src/components/Dots.tsx
@@ -4,17 +4,18 @@ interface DotsComponentProps {
   itemsLength: number;
   selectedIndex: number;
 }
+
 const Dots = ({ itemsLength, selectedIndex }: DotsComponentProps) => {
   const arr = new Array(itemsLength).fill(0);
 
   return (
-    <div className="my-2 hidden -translate-y-5 justify-center gap-1 md:flex">
+    <div className="my-2 flex -translate-y-5 justify-center gap-1">
       {arr.map((_, index) => {
         const selected = index === selectedIndex;
         return (
           <div
             className={classNames({
-              "h-2 w-2 rounded-full bg-blue-400 transition-all duration-300":
+              "h-1 w-1 rounded-full bg-blue-400 transition-all duration-300":
                 true,
               "opacity-50": !selected,
             })}
