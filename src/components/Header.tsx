22f1169c34f5f750086cad000c369e44857951db
Tiago Araujo <tiagomuniz130@gmail.com>
2023-12-11 07:52:02 -0300

    feat: create link on logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import { NavBar } from "./NavBar";
 import { ThemeSwitch } from "./ThemeSwitch";
 
@@ -7,7 +9,9 @@ const Header = () => {
       <div className="mx-auto flex w-full max-w-[1280px] items-center justify-between">
         <div className="flex items-center justify-center">
           <span className="mr-2 text-base font-extrabold text-gray-900 dark:text-white sm:text-xl">
-            <span className="text-blue-400">~</span>/tma_blog $
+            <Link href="/">
+              <span className="text-blue-400">~</span>/tma_blog $
+            </Link>
           </span>
           <span className="cursor-block" />
         </div>
