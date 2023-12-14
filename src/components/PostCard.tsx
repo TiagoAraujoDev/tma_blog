b09e4b02b5c1e91fd4a9fdcb7e7df06a54194227
Tiago Araujo <tiagomuniz130@gmail.com>
2023-12-14 16:33:27 -0300

    feat: Link to post

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,12 +1,13 @@
 import { Blog } from "contentlayer/generated";
 import { format, parseISO } from "date-fns";
 import Link from "next/link";
+import { FaArrowRight } from "react-icons/fa";
 
 const PostCard: React.FC<Blog> = (post) => {
   return (
     <div
       className="
-      mb-8 flex w-full items-center overflow-hidden rounded border
+      relative mb-8 flex w-full items-center overflow-hidden rounded border
       border-gray-500 bg-stone-300/30 shadow-lg shadow-gray-700 backdrop-blur-sm
       hover:border-blue-500 dark:bg-stone-700/30 dark:shadow-lg dark:shadow-black
       "
@@ -15,8 +16,11 @@ const PostCard: React.FC<Blog> = (post) => {
         <img className="object-contain" src={post.imageUrl} alt="" />
       </div>
       <div className="w-52 flex-1 px-4 py-2">
-        <h2 className="truncate text-xl text-gray-900 hover:text-blue-600 dark:text-gray-100">
-          <Link href={post.url} className="font-bold">
+        <h2 className="truncate text-xl text-gray-900 dark:text-gray-100">
+          <Link
+            href={post.url}
+            className="font-bold transition hover:text-blue-500"
+          >
             {post.title}
           </Link>
         </h2>
@@ -40,6 +44,16 @@ const PostCard: React.FC<Blog> = (post) => {
           {format(parseISO(post.publishedAt), "LLLL d, yyyy")}
         </time>
       </div>
+      <Link
+        href={post.url}
+        className="
+        absolute bottom-3 right-4 flex h-4 items-center justify-center gap-2
+        transition hover:text-blue-500
+        "
+      >
+        <span className="text-sm font-bold">Read</span>
+        <FaArrowRight className="h-3 w-3" />
+      </Link>
     </div>
   );
 };
