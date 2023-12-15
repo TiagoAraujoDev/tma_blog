4568a9f44f88682312e3354c54ba7eeaeaab49ee
Tiago Araujo <tiagomuniz130@gmail.com>
2023-12-15 08:12:42 -0300

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,26 @@ export const metadata: Metadata = {
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head></head>
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </head>
       <body
         className={`${space_grotesk.className} overflow-x-hidden scroll-smooth bg-gray-50 dark:bg-gray-950`}
       >
